197f2713758d7422a8a8e20638495e7bced6b5cb
Patrick <163540237+Patty-Github@users.noreply.github.com>
2025-08-27 23:42:42 +0930

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
 import os
 
+# 3 things per item in list. name of file format, number of that format, size of that format.
+formatInfo = []
+
 
 def get_folder_size(folder):
     folder_size = 0
     for dirpath, dirnames, filenames in os.walk(folder):
         #print("dirpath:", dirpath, "filenames:", filenames)
+
+        for g in filenames:
+            fileType = g[g.rfind('.'):]
+            # if format isn't in list, add format to list, add 1 to format's quantNum, add fileSize
+            if not any(fileType in g for g in formatInfo):
+                print("format is not in")
+                formatInfo.append((fileType, 1, os.path.getsize(dirpath)))
+
+            else:
+                # add 1 to format's quantNum, add size to fileSize
+
+                indexOfFileType = 0
+                # get index of fileType
+                for index, l in enumerate(formatInfo):
+                    if fileType in l:
+                        indexOfFileType = index
+                        print(fileType, index, l)
+
+                # convert to list when needed
+                print("formatinfo", formatInfo)
+                formatInfoList = list(formatInfo)
+                formatInfoListInner = list(formatInfoList[indexOfFileType])
+                formatInfoListInner[1] += 1
+                formatInfoList[indexOfFileType] = tuple(formatInfoListInner)
+                formatInfo = tuple(formatInfoList)
+
+                # find fileType in list, then add 1 to index 1
+
+
+
+            #print("G", g[g.rfind('.'):])
+            #formatInfo.append(f"{g[g.rfind('.'):]}")
+
         for f in filenames:
             fullPath = os.path.join(dirpath, f)
             if not os.path.islink(fullPath):
                 folder_size += os.path.getsize(fullPath)
 
+    print("NUM OF FORMAT", formatInfo)
     return folder_size
 
 
@@ -39,7 +76,7 @@ for i in allFolders:
     allFolderNamesAndSizes.append((currentDir, str(thisFolderSizeMB)[:dotPos]))
     #limit number of folders scanned
     numScanned = numScanned + 1
-    if numScanned >= 24:
+    if numScanned >= 9:
         print("Scan Finished")
         break
 
@@ -58,8 +95,7 @@ for item in sortedFolders:
 
 print(f"Scanned {totalMB}MB or {totalMB / 1000}GB")
 
-# get size of all folders in C drive
-
+#
 
 '''
 
